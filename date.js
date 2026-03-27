1398615093da151faaa6d64771faa8f71ec49fd9
vanita333 <vanitacharankar@gmail.com>
2026-03-27 16:46:34 +0530

    Added new JavaScript code

---
+++ date.js
@@ -0,0 +1,9 @@
+let d = new Date();
+
+console.log("Year:", d.getFullYear());
+console.log("Month:", d.getMonth() + 1); // Months start from 0
+console.log("Date:", d.getDate());
+console.log("Day:", d.getDay()); // 0 = Sunday
+console.log("Hours:", d.getHours());
+console.log("Minutes:", d.getMinutes());
+console.log("Seconds:", d.getSeconds());
