5f76e8d977d0215c72a2f3e7f888c5d43bbe6dc3
vanita333 <vanitacharankar@gmail.com>
2026-04-02 15:59:11 +0530

    update

---
+++ BookObject.js
@@ -0,0 +1,12 @@
+let book = {
+    title: "Java Programming",
+    author: "James Gosling",
+    price: 500,
+    publisher: "Oracle"
+};
+
+// Print details
+console.log("Title:", book.title);
+console.log("Author:", book.author);
+console.log("Price:", book.price);
+console.log("Publisher:", book.publisher);
